3722dcf361b3f1001d9251fd4d21c1517193dbb8
vishnu2410 <vishnu.priyansh@gmail.com>
2015-08-02 22:27:57 +0530

    Create handshake.cpp

---
+++ handshake.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int T,i;
+    cin>>T;
+    for(i=0;i<T;i++)
+    {
+        int n;
+        cin>>n;
+        cout<<n*(n-1)/2<<endl;
+    }
+    return 0;
+}
